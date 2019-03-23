baa2d3045c67bf3f84d63d9c2f6a6ee6c191392b
Aidan Cookson <aidanucookson@gmail.com>
2019-03-23 12:12:11 -0400

    Added Downsamping

--- image_to_audio.py
+++ image_to_audio.py
@@ -1,4 +1,5 @@
 import cv2
+import midi
 from os import getcwd, listdir, rename
 from os.path import isfile, join
 
@@ -7,10 +8,80 @@ MEDIA_FOLDER = "C:\\Users\\Aidan\\Documents\\git_repos\\media\\"
 
 
 
-def process_image(image_file):
+def process_video(video_file):
+    notes = []
+    
+    cap = cv2.VideoCapture(video_file)
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+
+        if (ret == False):
+            break
+
+        note = process_image(frame)
+        notes.append(note)
+       # print (note)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return notes
+
+def generate_midi_track(file_name, notes):
+    pattern = midi.Pattern()
+    track = midi.Track()
+    pattern.append(track)
+
+    for note in notes:
+        on = midi.NoteOnEvent(tick = 0, velocity = note[1], pitch = note[0])
+        off = midi.NoteOnEvent(tick = 60, velocity = 0, pitch = note[0])
+        track.append(on)
+        track.append(off) 
+
+
+    track.append(midi.EndOfTrackEvent(tick=1))
+    midi.write_midifile(file_name, pattern)
+
+def downsample_notes(notes, factor):
+    new_notes = []
+    remainder = len(notes) % factor
+
+    last_note = notes[len(notes)-1]
+    for _ in range(remainder):
+        notes.append(last_note)
+
+    accum_pitch = 0
+    accum_velocity = 0
+
+    for i in range(len(notes)):
+        note = notes[i]
+        accum_pitch += note[0]
+        accum_velocity += note[1]
+        if (i % factor == 0):
+            new_notes.append([int(accum_pitch / float(factor)), int(accum_velocity / float(factor))])
+            accum_pitch = 0
+            accum_velocity = 0
+    new_notes.append([int(accum_pitch / float(factor)), int(accum_velocity / float(factor))])
+    return new_notes
+        
+    
+
+def process_all_images():
+    cwd_media = MEDIA_FOLDER
+    media = [cwd_media + f for f in listdir(cwd_media) if isfile(join(cwd_media, f))]
+    for f in media:
+        img = cv2.imread(f)
+        note = process_image(img)
+        print (f)
+        print ("\t{}".format(note))
+
+def process_image(img):
 
     try:
-        img = cv2.imread(image_file)
         #cv2.imshow("Test", img)
         #if (img == None):
        #     return None
@@ -38,9 +109,11 @@ def rename_files(media):
         count += 1
 
 if __name__=="__main__":
-    cwd_media = MEDIA_FOLDER
-    media = [cwd_media + f for f in listdir(cwd_media) if isfile(join(cwd_media, f))]
-    for f in media:
-        note = process_image(f)
-        print (f)
-        print ("\t{}".format(note))
+    notes = process_video(MEDIA_FOLDER + '\\videos\\fools_gold.mp4')
+    new_notes = downsample_notes(notes, 8)
+    generate_midi_track("fools_gold_factor8_cont.mid", new_notes)
+##    pattern = midi.read_midifile("fools_gold_factor8.mid")
+##    track = pattern[0]
+##
+##    print (len(track))
+    
